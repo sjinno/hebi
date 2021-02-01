ba0d042314f5e951ea2dc13a7c439c995a17a4f2
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-31 22:09:16 -0800

    WIP feat: Add path finding method.

--- src/maze.rs
+++ src/maze.rs
@@ -1,4 +1,5 @@
 use rand::{self, Rng};
+use std::collections::VecDeque;
 use termion::color::{self, Green, Magenta, Reset, Yellow};
 
 #[derive(Clone)]
@@ -70,3 +71,125 @@ impl Maze {
 }
 
 struct Coord(usize, usize);
+
+// PATH FINDING METHOD
+fn update_queues(
+    maze: &mut Vec<Vec<String>>,
+    pos_queue: &mut VecDeque<(usize, usize)>,
+    path_queue: &mut VecDeque<String>,
+    paths: &mut Vec<String>,
+    path: String,
+    direction: char,
+    row: usize,
+    col: usize,
+    target: String,
+) {
+    let p = &maze[row][col];
+    if p == &target {
+        paths.push(format!("{}{}", path, direction));
+    } else if p == " " {
+        maze[row][col] = format!("{} {}", color::Fg(Yellow), color::Fg(Reset));
+        pos_queue.push_back((row, col));
+        path_queue.push_back(format!("{}{}", path, direction));
+    }
+}
+
+pub fn find_paths(
+    mut maze: Vec<Vec<String>>,
+    start_info: (usize, usize, usize, usize),
+    target: String,
+) -> Vec<String> {
+    let (row, col, row_len, col_len) = start_info;
+    let mut pos_queue = VecDeque::<(usize, usize)>::new();
+    let mut path_queue = VecDeque::<String>::new();
+    pos_queue.push_back((row, col));
+    path_queue.push_back("S".to_string());
+
+    let mut paths = vec![];
+    while let Some(pos) = pos_queue.pop_front() {
+        // println!("{:?}", pos);
+        let (r, c) = pos;
+        let path = path_queue.pop_front().unwrap();
+        let (row, col) = (r.clone(), c.clone());
+        if row > 0 {
+            let up = row - 1;
+            update_queues(
+                &mut maze,
+                &mut pos_queue,
+                &mut path_queue,
+                &mut paths,
+                path.clone(),
+                'U',
+                up,
+                col,
+                target.clone(),
+            );
+        }
+        if row < row_len - 1 {
+            let down = row + 1;
+            update_queues(
+                &mut maze,
+                &mut pos_queue,
+                &mut path_queue,
+                &mut paths,
+                path.clone(),
+                'D',
+                down,
+                col,
+                target.clone(),
+            );
+        }
+        if col > 0 {
+            let left = col - 1;
+            update_queues(
+                &mut maze,
+                &mut pos_queue,
+                &mut path_queue,
+                &mut paths,
+                path.clone(),
+                'L',
+                row,
+                left,
+                target.clone(),
+            );
+        }
+        if col < col_len - 1 {
+            let right = col + 1;
+            update_queues(
+                &mut maze,
+                &mut pos_queue,
+                &mut path_queue,
+                &mut paths,
+                path.clone(),
+                'R',
+                row,
+                right,
+                target.clone(),
+            );
+        }
+    }
+
+    paths
+}
+
+// pub fn draw(path: String, maze: Vec<Vec<String>>, start_info: (usize, usize, usize, usize)) {
+//     let mut m = maze.clone();
+//     let (mut r, mut c, _, _) = start_info;
+//     for p in path.chars() {
+//         match p {
+//             'U' => r -= 1,
+//             'D' => r += 1,
+//             'L' => c -= 1,
+//             'R' => c += 1,
+//             _ => {}
+//         }
+//         m[r][c] = "■".to_string();
+//     }
+
+//     for row in m {
+//         for col in row {
+//             print!("{} ", col);
+//         }
+//         println!();
+//     }
+// }
