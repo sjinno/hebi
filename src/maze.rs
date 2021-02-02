79c20a17cc2e902c92d27acfc91480d9ea468e95
Shohei Jinno <shohei.jinno@gmail.com>
2021-02-01 22:07:58 -0800

    Reviewed.

--- src/maze.rs
+++ src/maze.rs
@@ -1,16 +1,46 @@
-use rand::{self, Rng};
+use perf_event::Builder;
+use rand::{self, Rng}; // rand = "0.8.3"
 use std::collections::VecDeque;
+use std::fmt;
 use std::thread;
 use std::time::Duration;
-use termion::color::{self, LightGreen, Reset, Yellow};
+use termion::color::{self, LightGreen, Reset, Yellow}; // termion = "1.5.6" // perf-event = "0.4"
 
 struct Coord(usize, usize);
 
 #[derive(Clone)]
 pub struct Maze {
-    maze: Vec<Vec<String>>,
+    maze: Vec<Vec<Square>>,
     m: usize,
     n: usize,
+}
+
+/// Contents of a square in the maze.
+///
+/// In practice, Rust will assign these the the numbers 0..4, and represent
+/// `Square` values as single bytes. `Copy` means that you don't need to use
+/// `clone`: they're sort of always cloned. `Copy` is convenient, but it's only
+/// permitted for simple types like this; you can't use it for something more
+/// complex, like `Maze`.
+#[derive(Clone, Copy, Eq, PartialEq)]
+enum Square {
+    Empty,
+    Visited,
+    Block,
+    Mouse,
+    Cheese,
+}
+
+/// Tell `println!` how `{}` should display `Square` values.
+impl fmt::Display for Square {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Square::Empty | Square::Visited => fmt.write_str(" "),
+            Square::Block => fmt.write_str("■"),
+            Square::Mouse => write!(fmt, "{}●{}", color::Fg(LightGreen), color::Fg(Reset)),
+            Square::Cheese => write!(fmt, "{}▲{}", color::Fg(Yellow), color::Fg(Reset)),
+        }
+    }
 }
 
 impl Maze {
@@ -18,10 +48,10 @@ impl Maze {
         let row_bound = m + 2;
         let col_bound = n + 2;
 
-        let mut maze = vec![vec![String::from(" "); col_bound]; row_bound];
+        let mut maze = vec![vec![Square::Empty; col_bound]; row_bound];
         let mut rng = rand::thread_rng();
 
-        let block = String::from("■");
+        let block = Square::Block;
         let num_of_blocks = 150;
 
         let mut count = 0;
@@ -50,28 +80,36 @@ impl Maze {
     }
 
     pub fn start_game_loop(&mut self) {
-        let mouse = self.place_object(format!("{}●{}", color::Fg(LightGreen), color::Fg(Reset)));
+        let mouse = self.place_object(Square::Mouse);
         let mut start_info = (mouse.0, mouse.1, self.m, self.n);
 
-        loop {
-            let cheese = self.place_object(format!("{}▲{}", color::Fg(Yellow), color::Fg(Reset)));
-            let path = find_paths(
-                self.maze.clone(),
-                start_info,
-                format!("{}▲{}", color::Fg(Yellow), color::Fg(Reset)),
-            )[0]
-            .clone(); // Oonly get the shortest path.
+        let mut counter = Builder::new()
+            .build()
+            .expect("failed to create instruction counter");
 
-            draw(path, self.maze.clone(), start_info);
+        loop {
+            let cheese = self.place_object(Square::Cheese);
+
+            counter.reset().expect("reset counter");
+            counter.enable().expect("enable counter");
+
+            let path = find_paths(self.maze.clone(), start_info, Square::Cheese); // Oonly get the shortest path.
+
+            let instructions = counter
+                .read()
+                .expect("error reading find_paths instruction count");
+            counter.disable().expect("disable counter");
+
+            draw(path, self.maze.clone(), start_info, instructions);
 
             thread::sleep(Duration::from_nanos(1));
-            self.maze[cheese.0][cheese.1] = String::from(" ");
-            self.maze[mouse.0][mouse.1] = String::from(" ");
+            self.maze[cheese.0][cheese.1] = Square::Empty;
+            self.maze[mouse.0][mouse.1] = Square::Empty;
             start_info = (cheese.0, cheese.1, self.m, self.n);
         }
     }
 
-    fn place_object(&mut self, obj: String) -> Coord {
+    fn place_object(&mut self, obj: Square) -> Coord {
         let mut rng = rand::thread_rng();
         let r = rng.gen_range(1..self.m - 1);
         let c = rng.gen_range(1..self.n - 1);
@@ -81,105 +119,117 @@ impl Maze {
 }
 
 fn find_paths(
-    mut maze: Vec<Vec<String>>,
+    mut maze: Vec<Vec<Square>>,
     start_info: (usize, usize, usize, usize),
-    target: String,
-) -> Vec<String> {
+    target: Square,
+) -> String {
     let (row, col, row_len, col_len) = start_info;
     let mut pos_queue = VecDeque::<(usize, usize)>::new();
     let mut path_queue = VecDeque::<String>::new();
     pos_queue.push_back((row, col));
     path_queue.push_back("S".to_string());
 
-    let mut paths = vec![];
     while let Some(pos) = pos_queue.pop_front() {
-        // println!("{:?}", pos);
         let (r, c) = pos;
+
         let path = path_queue.pop_front().unwrap();
         let (row, col) = (r, c);
         if row > 0 {
             let up = row - 1;
-            update_queues(
+            let path = update_queues(
                 &mut maze,
                 &mut pos_queue,
                 &mut path_queue,
-                &mut paths,
                 path.clone(),
                 'U',
                 up,
                 col,
                 target.clone(),
             );
+            if let Some(p) = path {
+                return p;
+            }
         }
         if row < row_len - 1 {
             let down = row + 1;
-            update_queues(
+            let path = update_queues(
                 &mut maze,
                 &mut pos_queue,
                 &mut path_queue,
-                &mut paths,
                 path.clone(),
                 'D',
                 down,
                 col,
                 target.clone(),
             );
+            if let Some(p) = path {
+                return p;
+            }
         }
         if col > 0 {
             let left = col - 1;
-            update_queues(
+            let path = update_queues(
                 &mut maze,
                 &mut pos_queue,
                 &mut path_queue,
-                &mut paths,
                 path.clone(),
                 'L',
                 row,
                 left,
                 target.clone(),
             );
+            if let Some(p) = path {
+                return p;
+            }
         }
         if col < col_len - 1 {
             let right = col + 1;
-            update_queues(
+            let path = update_queues(
                 &mut maze,
                 &mut pos_queue,
                 &mut path_queue,
-                &mut paths,
                 path.clone(),
                 'R',
                 row,
                 right,
                 target.clone(),
             );
+            if let Some(p) = path {
+                return p;
+            }
         }
     }
 
-    paths
+    String::new()
 }
 
 fn update_queues(
-    maze: &mut Vec<Vec<String>>,
+    maze: &mut Vec<Vec<Square>>,
     pos_queue: &mut VecDeque<(usize, usize)>,
     path_queue: &mut VecDeque<String>,
-    paths: &mut Vec<String>,
     path: String,
     direction: char,
     row: usize,
     col: usize,
-    target: String,
-) {
-    let p = &maze[row][col];
-    if p == &target {
-        paths.push(format!("{}{}", path, direction));
-    } else if p == " " {
-        maze[row][col] = format!("{} {}", color::Fg(Yellow), color::Fg(Reset));
+    target: Square,
+) -> Option<String> {
+    let p = maze[row][col];
+    if p == target {
+        return Some(format!("{}{}", path, direction));
+    } else if p == Square::Empty {
+        maze[row][col] = Square::Visited;
         pos_queue.push_back((row, col));
         path_queue.push_back(format!("{}{}", path, direction));
     }
+    None
 }
 
-fn draw(path: String, maze: Vec<Vec<String>>, start_info: (usize, usize, usize, usize)) {
+fn draw(
+    path: String,
+    maze: Vec<Vec<Square>>,
+    start_info: (usize, usize, usize, usize),
+    instructions: u64,
+) {
     let mut m = maze;
     let (mut r, mut c, _, _) = start_info;
     let mut snake = VecDeque::<(usize, usize)>::new();
@@ -188,7 +238,7 @@ fn draw(path: String, maze: Vec<Vec<String>>, start_info: (usize, usize, usize, 
             snake.push_back((r, c));
         } else {
             let (tail_r, tail_c) = snake.pop_front().unwrap();
-            m[tail_r][tail_c] = String::from(" ");
+            m[tail_r][tail_c] = Square::Empty;
             snake.push_back((r, c));
         }
         match p {
@@ -198,13 +248,15 @@ fn draw(path: String, maze: Vec<Vec<String>>, start_info: (usize, usize, usize, 
             'R' => c += 1,
             _ => {}
         }
-        m[r][c] = format!("{}●{}", color::Fg(LightGreen), color::Fg(Reset));
+        m[r][c] = Square::Mouse;
         for row in &m {
             for col in row {
                 print!("{} ", col);
             }
             println!();
         }
+        println!("instructions to find paths: {}", instructions);
+
         thread::sleep(Duration::from_millis(60));
         println!("\x1B[2J\x1B[1;1H");
     }
